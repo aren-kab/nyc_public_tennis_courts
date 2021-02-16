73f37492c770563a53a0b5510c80d1bf0f8b61d1
aren-kab <akabarajian@gmail.com>
2021-02-16 13:41:27 -0500

    push

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,40 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXJlbi1rYWIiLCJhIjoiY2tsMTJhejk3MHhxazJxcW5sb
   center: [-73.99, 40.70], // starting position [lng, lat]
   zoom: 11 // starting zoom
   });
+
+  // let's add the tennis courts to the map!!!
+
+  $.getJSON('./data/tennis_courts.json', function(courts) {
+    console.log(courts)
+
+    pizzaRows.forEach(function(court) {
+      console.log(court.Name, court.Prop_ID)
+
+      var html = `
+        <div>
+          <h3>${court.Name}</h3>
+          <div>Location: ${court.Location}</div>
+          <div>Type: ${court.Tennis_Type}</div>
+          <div><i>"${court.Info}"</i></div>
+        </div>
+      `
+
+      // all non MUP and CUSP will be this color
+      var color = 'green'
+
+      if (court.Courts > 4 && court.Courts < 8) {
+        color = 'yellow'
+      }
+
+      if (court.Courts > 8) {
+        color = 'orange'
+      }
+
+      new mapboxgl.Marker({
+        color: color
+      })
+        .setLngLat([court.lon, court.lat])
+        .setPopup(new mapboxgl.Popup().setHTML(html)) // add popup
+        .addTo(map);
+    })
+  })
